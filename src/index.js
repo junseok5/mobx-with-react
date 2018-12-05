b54495d1a02d0983037b1a331c25308fe9e1347a
junseok5 <vkehrkrj@naver.com>
2018-12-05 20:11:08 +0900

    completed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { Provider } from 'mobx-react'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import CounterStore from './stores/counter'
+import RootStore from './stores'
 
-const counter = new CounterStore()
+const root = new RootStore()
 
 ReactDOM.render(
-  <Provider counter={counter}>
+  <Provider {...root}>
     <App />
   </Provider>,
   document.getElementById('root')
